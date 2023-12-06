c0f71418715406c3767438bfec6265aea0d854fa
aza72 <mavtodru@mail.ru>
2023-12-06 07:49:46 +0500

    home commot

--- store/urls.py
+++ store/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     path('cancel/', CancelView.as_view(), name='cancel'),
     path('success/', SuccessView.as_view(), name='success'),
     path('', HomePageView.as_view(), name='home'),
-    #path('item/<int:pk>', ItemPageView.as_view(), name='item'),
-    #path('buy/<int:pk>/', CreateCheckoutSessionView.as_view(), name='create-checkout-session')
+    path('item/<int:pk>', ItemPageView.as_view(), name='item'),
+    path('buy/<int:pk>/', CreateCheckoutSessionView.as_view(), name='create-checkout-session')
 ]
