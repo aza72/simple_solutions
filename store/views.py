48cd1951762900d3285de371d505e48819c989d5
Anatolii <mavtodrug@mail.ru>
2023-12-06 11:11:18 +0500

    finish

--- store/views.py
+++ store/views.py
@@ -7,6 +7,7 @@ from django.views.generic import TemplateView
 
 from .models import Item
 
+stripe.api_key = settings.STRIPE_SECRET_KEY
 class HomePageView(TemplateView):
     template_name = "store/home.html"
 
@@ -30,9 +31,46 @@ class CancelView(TemplateView):
 class ItemPageView(TemplateView):
     template_name = "store/item.html"
 
+    def get_context_data(self, **kwargs):
+        pk = self.kwargs.get("pk")
+        product = Item.objects.get(pk=pk)
+        context = super(ItemPageView, self).get_context_data(**kwargs)
+        context.update({
+            "product": product,
+            "STRIPE_PUBLIC_KEY": settings.STRIPE_PUBLIC_KEY
+        })
+        return context
 
 
-
+class CreateCheckoutSessionView(View):
+    def post(self, request, *args, **kwargs):
+        product_id = self.kwargs["pk"]
+        product = Item.objects.get(id=product_id)
+        YOUR_DOMAIN = "http://127.0.0.1:8000"
+        checkout_session = stripe.checkout.Session.create(
+            payment_method_types=['card'],
+            line_items=[
+                {
+                    'price_data': {
+                        'currency': 'usd',
+                        'unit_amount': product.price*100,
+                        'product_data': {
+                            'name': product.name,
+                        },
+                    },
+                    'quantity': 1,
+                },
+            ],
+            metadata={
+                "product_id": product.id
+            },
+            mode='payment',
+            success_url=YOUR_DOMAIN + '/success/',
+            cancel_url=YOUR_DOMAIN + '/cancel/',
+        )
+        return JsonResponse({
+            'id': checkout_session.id
+        })
 
 
 
